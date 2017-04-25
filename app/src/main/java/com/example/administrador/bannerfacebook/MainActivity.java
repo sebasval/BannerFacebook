951c0925ef39b245c2dfc62b6b034805ccbb53e8
sebasval <s.dvh27@gmail.com>
2017-04-25 15:50:37 -0500

    examen 12

---
+++ app/src/main/java/com/example/administrador/bannerfacebook/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.administrador.bannerfacebook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
