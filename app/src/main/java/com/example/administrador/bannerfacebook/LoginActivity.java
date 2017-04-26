7f98be960adb944b9fc6845eb344581b67194f17
sebasval <s.dvh27@gmail.com>
2017-04-26 13:15:06 -0500

    Examen Unidad 12

--- app/src/main/java/com/example/administrador/bannerfacebook/LoginActivity.java
+++ app/src/main/java/com/example/administrador/bannerfacebook/LoginActivity.java
@@ -12,6 +12,7 @@ import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 public class LoginActivity extends AppCompatActivity {
 
